015d957d0e5c73b6e8438012a2f3b3832714f58a
martines328 <55957474+martines328@users.noreply.github.com>
2019-11-27 11:28:13 +0200

    kotlin lang

--- app/src/main/java/com/example/newsapi/Api/ApiClient.kt
+++ app/src/main/java/com/example/newsapi/Api/ApiClient.kt
@@ -1,6 +1,8 @@
 package com.example.newsapi.Api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiClient {
 
@@ -9,10 +11,20 @@ companion object{
 
     val basicApi : String = "https://newsapi.org/v2/"
     lateinit var  retrofit : Retrofit
+    val okHttpBuilder = OkHttpClient.Builder()
+    
+    
+    fun getApiClient() : Retrofit = Retrofit.Builder()
+                .baseUrl(basicApi)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpBuilder.build())
+                .build()
+            
+        
+    }
 
 
 
 }
 
 
-}
--- app/src/main/java/com/example/newsapi/MainActivity.kt
+++ app/src/main/java/com/example/newsapi/MainActivity.kt
@@ -1,12 +1,102 @@
 package com.example.newsapi
 
+import android.content.Context
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.newsapi.Api.ApiClient
+import com.example.newsapi.Api.ApiInterface
+import com.example.newsapi.Api.Arcticle
+import com.example.newsapi.Api.News
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+
+    companion object{
+
+        val sources : String = "techcrunch"
+        val apiKey : String = "156885bf94af406cbe310eb5ef39d6da"
+        val TAG : String = "mytag"
+
+
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Log.d(TAG, "oncreate")
+        getNews()
+
     }
+
+
+
+
+    fun getNews() {
+
+        val apiClient = ApiClient
+        var arcticles : List<Arcticle>
+        Log.d(TAG, "getNews")
+        
+        doAsync {
+
+            Log.d(TAG, "doAsync")
+
+            val apiInterface : ApiInterface = apiClient.getApiClient().create(ApiInterface::class.java)
+            val call : Call<News> = apiInterface.getNewsSearch(sources, apiKey)
+
+            call.enqueue(object : Callback<News>{
+
+                override fun onFailure(call: Call<News>, t: Throwable) {
+                
+                    toast("Please choose your request")
+                    Log.d(TAG, "onfailure")
+                }
+
+                override fun onResponse(call: Call<News>, response: Response<News>) {
+                
+                    arcticles = response.body()!!.arcticles
+                    Log.d(TAG, "on Response")
+                    
+                    for ( i in arcticles){
+                        var arcticle : Arcticle = arcticles.get(5)
+                        var title : String = arcticle.title
+                        Log.d(TAG, title)
+
+
+
+
+                    }
+                
+                
+                }
+            })
+            
+
+
+
+            
+
+
+
+        }
+        
+        
+
+
+    }
+
+
+
 }
